c06b94e22284d186a425d7b59812626751da01e1
Saliman Rahmani <salimanr@gmail.com>
2023-08-21 16:22:56 -0700

    updated Karate

---
+++ src/test/java/api/runner/RegressionRunner.java
@@ -0,0 +1,17 @@
+package api.runner;
+import com.intuit.karate.junit5.Karate;
+
+public class RegressionRunner {
+
+
+
+
+		@Karate.Test
+		public Karate runTest() {
+		
+		return Karate.run("classpath:features")
+				.tags("Regression");
+		
+		
+	}
+	}
